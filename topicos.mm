2aa0bbb63365b756fd3b8663fb40dd708443be11
thiagopas <38135428+thiagopas@users.noreply.github.com>
2020-12-09 11:00:00 -0300

    Adição do mapa mental de tópicos

---
+++ topicos.mm
@@ -0,0 +1,178 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1606963364896" ID="ID_1941080162" MODIFIED="1606963543321" STYLE="bubble" TEXT="T&#xf3;picos">
+<node CREATED="1606963590938" FOLDED="true" ID="ID_1728845974" MODIFIED="1607518505457" POSITION="right" TEXT="Otimiza&#xe7;&#xe3;o">
+<node CREATED="1606964041319" ID="ID_1026839471" LINK="https://web.stanford.edu/~boyd/cvxbook/bv_cvxbook.pdf" MODIFIED="1606964230909">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Convex Optimization (Boyd &amp; Vandenberghe)<br />(Livro em PDF)<br />
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1607518523431" FOLDED="true" ID="ID_932147248" MODIFIED="1607518952975" POSITION="right" TEXT="Ac&#xfa;stica e Ultrassom">
+<node CREATED="1607518803639" FOLDED="true" ID="ID_1410858501" MODIFIED="1607518950451" TEXT="Spatial impulse response">
+<node CREATED="1607518856783" ID="ID_799823470" LINK="http://bme.elektro.dtu.dk/31545/notes/ref_jaj_2015.pdf" MODIFIED="1607518939691">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Notes for the International Summer School on Advanced Ultrasound Imaging<br />(J&#248;rgen Arendt Jensen)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1606964165141" FOLDED="true" ID="ID_920771390" MODIFIED="1607520228741" POSITION="right" TEXT="Sinais e Sistemas">
+<node CREATED="1607517938795" HGAP="53" ID="ID_1585418671" LINK="https://www.youtube.com/watch?v=KJnAy6hzetw&amp;list=PL41692B571DD0AF9B&amp;ab_channel=MITOpenCourseWare" MODIFIED="1607517991581" VSHIFT="20">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Signals and Systems
+    </p>
+    <p>
+      (Allan Oppenheim - Youtube)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1607518212953" FOLDED="true" HGAP="56" ID="ID_1365932305" MODIFIED="1607518483983" TEXT="S&#xe9;ries de Fourier" VSHIFT="16">
+<node CREATED="1607518223795" ID="ID_658626383" LINK="https://lpsa.swarthmore.edu/Fourier/Series/ExFS.html" MODIFIED="1607518246704">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Fourier Series Examples
+    </p>
+    <p>
+      (Swarthmore)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1606964173899" FOLDED="true" HGAP="49" ID="ID_648621551" MODIFIED="1607518477263" TEXT="Diagramas de Bode" VSHIFT="21">
+<node CREATED="1607518286946" ID="ID_765408096" LINK="https://lpsa.swarthmore.edu/Bode/BodeHow.html" MODIFIED="1607518337919">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The Asymptotic Bode Diagram: Derivation of Approximations<br />(Swarthmore)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1607518357239" ID="ID_1501815572" LINK="https://lpsa.swarthmore.edu/Bode/BodeExamples.html" MODIFIED="1607518375428">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Bode Plot Examples
+    </p>
+    <p>
+      (Swarthmore)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1607518407584" FOLDED="true" ID="ID_410213140" MODIFIED="1607518481870" TEXT="Transformada de Laplace">
+<node CREATED="1606964189651" FOLDED="true" ID="ID_85164201" MODIFIED="1607518478491" TEXT="Fra&#xe7;&#xf5;es parciais para Transf. de Laplace">
+<node CREATED="1607518426514" ID="ID_207241993" LINK="https://lpsa.swarthmore.edu/BackGround/PartialFraction/PartialFraction.html" MODIFIED="1607518447050" TEXT="Partial Fraction Expansion"/>
+</node>
+</node>
+</node>
+<node CREATED="1606963560544" FOLDED="true" HGAP="22" ID="ID_81845382" MODIFIED="1607520262120" POSITION="right" TEXT="Identifica&#xe7;&#xe3;o de Sistemas" VSHIFT="-1">
+<node CREATED="1606963608382" ID="ID_1724472532" LINK="https://www.youtube.com/watch?v=TWdgSG0sMlQ&amp;list=PLALrL4i0Pz6DrrCkkJ-k-_S3qi1bFzUUu&amp;ab_channel=LuisAntonioAguirre" MODIFIED="1606964315043" TEXT="Modelagem e Identifica&#xe7;&#xe3;o&#xa;(Luis Fernando Aguirre - Youtube)"/>
+</node>
+<node CREATED="1606964273870" FOLDED="true" ID="ID_406252487" MODIFIED="1607520272209" POSITION="right" TEXT="Controladores L&#xf3;gicos Program&#xe1;veis">
+<node CREATED="1606964284979" FOLDED="true" ID="ID_62288926" MODIFIED="1607520264553" TEXT="CodeSys">
+<node CREATED="1607520173718" ID="ID_1524829885" LINK="https://www.youtube.com/watch?v=78W8Kf8GLAk&amp;ab_channel=GVensino" MODIFIED="1607520256763">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Tutorial CodeSys
+    </p>
+    <p>
+      (Gabriel V. Silva Maganha - Youtube)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1606963851473" FOLDED="true" ID="ID_226930671" MODIFIED="1607518489946" POSITION="right" TEXT="C&#xe1;lculo">
+<node CREATED="1606963900774" FOLDED="true" ID="ID_1325040626" MODIFIED="1607514573322" TEXT="C&#xe1;lculo multivari&#xe1;vel">
+<node CREATED="1606963857208" ID="ID_25741876" LINK="https://www.khanacademy.org/math/multivariable-calculus/applications-of-multivariable-derivatives#quadratic-approximations" MODIFIED="1606963965720">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Quadratic Representations<br />(Khan Academy - Youtube)<br />
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1606963917192" FOLDED="true" ID="ID_1274649913" MODIFIED="1607518488538" TEXT="C&#xe1;lculo Variacional">
+<node CREATED="1606963926639" ID="ID_1391634991" LINK="https://www.youtube.com/watch?v=6HeQc7CSkZs&amp;ab_channel=FacultyofKhan" MODIFIED="1606963990961">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Calculus of Variations
+    </p>
+    <p>
+      (Faculty of Khan - Youtube)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1606964514778" ID="ID_588094371" LINK="https://drive.google.com/file/d/1jg3eNaQx_T9ZsljRUR4bFFWpQZe6o0mu/view" MODIFIED="1606964556289">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      A practical introduction to Calculus of Variations
+    </p>
+    <p>
+      (Thiago Passarin - v&#237;deo)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
