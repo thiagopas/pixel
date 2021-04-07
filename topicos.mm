3ed8c0481a21eeb350c4fcd02c46c8ad9815ad52
thiagopas <38135428+thiagopas@users.noreply.github.com>
2021-04-07 09:49:53 -0300

    Add files via upload

--- topicos.mm
+++ topicos.mm
@@ -1,7 +1,7 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1606963364896" ID="ID_1941080162" MODIFIED="1606963543321" STYLE="bubble" TEXT="T&#xf3;picos">
-<node CREATED="1606963590938" FOLDED="true" ID="ID_1728845974" MODIFIED="1607518505457" POSITION="right" TEXT="Otimiza&#xe7;&#xe3;o">
+<node CREATED="1606963590938" FOLDED="true" ID="ID_1728845974" MODIFIED="1617798967164" POSITION="right" TEXT="Otimiza&#xe7;&#xe3;o">
 <node CREATED="1606964041319" ID="ID_1026839471" LINK="https://web.stanford.edu/~boyd/cvxbook/bv_cvxbook.pdf" MODIFIED="1606964230909">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -15,7 +15,7 @@
 </html></richcontent>
 </node>
 </node>
-<node CREATED="1607518523431" FOLDED="true" ID="ID_932147248" MODIFIED="1607518952975" POSITION="right" TEXT="Ac&#xfa;stica e Ultrassom">
+<node CREATED="1607518523431" FOLDED="true" ID="ID_932147248" MODIFIED="1617799212382" POSITION="right" TEXT="Ac&#xfa;stica e Ultrassom">
 <node CREATED="1607518803639" FOLDED="true" ID="ID_1410858501" MODIFIED="1607518950451" TEXT="Spatial impulse response">
 <node CREATED="1607518856783" ID="ID_799823470" LINK="http://bme.elektro.dtu.dk/31545/notes/ref_jaj_2015.pdf" MODIFIED="1607518939691">
 <richcontent TYPE="NODE"><html>
@@ -27,8 +27,7 @@
       Notes for the International Summer School on Advanced Ultrasound Imaging<br />(J&#248;rgen Arendt Jensen)
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
@@ -62,8 +61,7 @@
       (Swarthmore)
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 <node CREATED="1606964173899" FOLDED="true" HGAP="49" ID="ID_648621551" MODIFIED="1607518477263" TEXT="Diagramas de Bode" VSHIFT="21">
@@ -77,8 +75,7 @@
       The Asymptotic Bode Diagram: Derivation of Approximations<br />(Swarthmore)
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1607518357239" ID="ID_1501815572" LINK="https://lpsa.swarthmore.edu/Bode/BodeExamples.html" MODIFIED="1607518375428">
 <richcontent TYPE="NODE"><html>
@@ -93,8 +90,7 @@
       (Swarthmore)
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 <node CREATED="1607518407584" FOLDED="true" ID="ID_410213140" MODIFIED="1607518481870" TEXT="Transformada de Laplace">
@@ -103,11 +99,11 @@
 </node>
 </node>
 </node>
-<node CREATED="1606963560544" FOLDED="true" HGAP="22" ID="ID_81845382" MODIFIED="1607520262120" POSITION="right" TEXT="Identifica&#xe7;&#xe3;o de Sistemas" VSHIFT="-1">
+<node CREATED="1606963560544" FOLDED="true" HGAP="22" ID="ID_81845382" MODIFIED="1617799218310" POSITION="right" TEXT="Identifica&#xe7;&#xe3;o de Sistemas" VSHIFT="-1">
 <node CREATED="1606963608382" ID="ID_1724472532" LINK="https://www.youtube.com/watch?v=TWdgSG0sMlQ&amp;list=PLALrL4i0Pz6DrrCkkJ-k-_S3qi1bFzUUu&amp;ab_channel=LuisAntonioAguirre" MODIFIED="1606964315043" TEXT="Modelagem e Identifica&#xe7;&#xe3;o&#xa;(Luis Fernando Aguirre - Youtube)"/>
 </node>
-<node CREATED="1606964273870" FOLDED="true" ID="ID_406252487" MODIFIED="1607520272209" POSITION="right" TEXT="Controladores L&#xf3;gicos Program&#xe1;veis">
-<node CREATED="1606964284979" FOLDED="true" ID="ID_62288926" MODIFIED="1607520264553" TEXT="CodeSys">
+<node CREATED="1606964273870" FOLDED="true" ID="ID_406252487" MODIFIED="1617799476401" POSITION="right" TEXT="Controladores L&#xf3;gicos Program&#xe1;veis">
+<node CREATED="1606964284979" FOLDED="true" ID="ID_62288926" MODIFIED="1617799475596" TEXT="CodeSys">
 <node CREATED="1607520173718" ID="ID_1524829885" LINK="https://www.youtube.com/watch?v=78W8Kf8GLAk&amp;ab_channel=GVensino" MODIFIED="1607520256763">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -121,13 +117,28 @@
       (Gabriel V. Silva Maganha - Youtube)
     </p>
   </body>
+</html></richcontent>
+</node>
+<node CREATED="1617799398265" ID="ID_1782215732" LINK="https://www.youtube.com/c/TohidAlizadeh/videos" MODIFIED="1617799444960">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      PLC Programming using CODESYS
+    </p>
+    <p>
+      (Tohid Alizadeh - YouTube)
+    </p>
+  </body>
 </html>
 </richcontent>
 </node>
 </node>
 </node>
-<node CREATED="1606963851473" FOLDED="true" ID="ID_226930671" MODIFIED="1607518489946" POSITION="right" TEXT="C&#xe1;lculo">
-<node CREATED="1606963900774" FOLDED="true" ID="ID_1325040626" MODIFIED="1607514573322" TEXT="C&#xe1;lculo multivari&#xe1;vel">
+<node CREATED="1606963851473" FOLDED="true" ID="ID_226930671" MODIFIED="1617799226181" POSITION="right" TEXT="C&#xe1;lculo">
+<node CREATED="1606963900774" FOLDED="true" ID="ID_1325040626" MODIFIED="1617799222237" TEXT="C&#xe1;lculo multivari&#xe1;vel">
 <node CREATED="1606963857208" ID="ID_25741876" LINK="https://www.khanacademy.org/math/multivariable-calculus/applications-of-multivariable-derivatives#quadratic-approximations" MODIFIED="1606963965720">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -140,8 +151,26 @@
   </body>
 </html></richcontent>
 </node>
+<node CREATED="1617798977470" ID="ID_1406229378" LINK="https://www.youtube.com/watch?v=wCZ1VEmVjVo" MODIFIED="1617799139164">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      What is Jacobian? | The right way of
+    </p>
+    <p>
+      thinking derivatives and integrals
+    </p>
+    <p>
+      (Mathemaniac - YouTube)
+    </p>
+  </body>
+</html></richcontent>
 </node>
-<node CREATED="1606963917192" FOLDED="true" ID="ID_1274649913" MODIFIED="1607518488538" TEXT="C&#xe1;lculo Variacional">
+</node>
+<node CREATED="1606963917192" FOLDED="true" ID="ID_1274649913" MODIFIED="1617799185656" TEXT="C&#xe1;lculo Variacional">
 <node CREATED="1606963926639" ID="ID_1391634991" LINK="https://www.youtube.com/watch?v=6HeQc7CSkZs&amp;ab_channel=FacultyofKhan" MODIFIED="1606963990961">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -172,6 +201,25 @@
   </body>
 </html></richcontent>
 </node>
+</node>
+</node>
+<node CREATED="1613997275996" FOLDED="true" ID="ID_1928652108" MODIFIED="1617799194602" POSITION="right" TEXT="Matlab">
+<node CREATED="1613997287096" ID="ID_1463047083" MODIFIED="1613997442687">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      GNU Octave
+    </p>
+    <p>
+      (Sistema em c&#243;digo aberto compat&#237;vel com Matlab)
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1613997296974" ID="ID_1711921887" LINK="https://octave-online.net/" MODIFIED="1613997330499" TEXT="Octave online"/>
+<node CREATED="1613997311414" ID="ID_239783632" LINK="https://en.wikipedia.org/wiki/GNU_Octave" MODIFIED="1613997375187" TEXT="GNU Octave (Wikipedia)"/>
 </node>
 </node>
 </node>
