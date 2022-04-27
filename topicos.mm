71179da7a025e580807d6d77e671585a4eac5526
Thiago A. R. Passarin <38135428+thiagopas@users.noreply.github.com>
2022-04-26 23:56:43 -0300

    Add files via upload

--- topicos.mm
+++ topicos.mm
@@ -1,7 +1,11 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1606963364896" ID="ID_1941080162" MODIFIED="1606963543321" STYLE="bubble" TEXT="T&#xf3;picos">
-<node CREATED="1606963590938" FOLDED="true" ID="ID_1728845974" MODIFIED="1617798967164" POSITION="right" TEXT="Otimiza&#xe7;&#xe3;o">
+<node CREATED="1606963590938" FOLDED="true" ID="ID_1728845974" MODIFIED="1649074738301" POSITION="right" TEXT="Otimiza&#xe7;&#xe3;o">
+<node CREATED="1648756912593" ID="ID_1761385891" MODIFIED="1648756917717" TEXT="Adjoint state methods">
+<node CREATED="1648756919745" ID="ID_1813753556" MODIFIED="1648756920526" TEXT="Granzow, Glen D. &quot;A tutorial on adjoint methods and their use for data assimilation in glaciology.&quot; Journal of Glaciology 60.221 (2014): 440-446."/>
+</node>
+<node CREATED="1648756926121" ID="ID_609667712" MODIFIED="1648756931502" TEXT="Convex optimization">
 <node CREATED="1606964041319" ID="ID_1026839471" LINK="https://web.stanford.edu/~boyd/cvxbook/bv_cvxbook.pdf" MODIFIED="1606964230909">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -15,8 +19,14 @@
 </html></richcontent>
 </node>
 </node>
-<node CREATED="1607518523431" FOLDED="true" ID="ID_932147248" MODIFIED="1617799212382" POSITION="right" TEXT="Ac&#xfa;stica e Ultrassom">
-<node CREATED="1607518803639" FOLDED="true" ID="ID_1410858501" MODIFIED="1607518950451" TEXT="Spatial impulse response">
+<node CREATED="1648756974041" ID="ID_95634564" MODIFIED="1648756982469" TEXT="Greedy algorithms">
+<node CREATED="1648757114124" ID="ID_289126857" MODIFIED="1648757136676" TEXT="Orthogonal Matching Pursuit (OMP)">
+<node CREATED="1648757127666" ID="ID_1099533201" MODIFIED="1648757128445" TEXT="Tropp, Joel A., and Anna C. Gilbert. &quot;Signal recovery from random measurements via orthogonal matching pursuit.&quot; IEEE Transactions on information theory 53.12 (2007): 4655-4666."/>
+</node>
+</node>
+</node>
+<node CREATED="1607518523431" FOLDED="true" ID="ID_932147248" MODIFIED="1651028044624" POSITION="right" TEXT="Ac&#xfa;stica e Ultrassom">
+<node CREATED="1607518803639" FOLDED="true" ID="ID_1410858501" MODIFIED="1649074764586" TEXT="Spatial impulse response">
 <node CREATED="1607518856783" ID="ID_799823470" LINK="http://bme.elektro.dtu.dk/31545/notes/ref_jaj_2015.pdf" MODIFIED="1607518939691">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -29,9 +39,21 @@
   </body>
 </html></richcontent>
 </node>
+<node CREATED="1648756207394" ID="ID_1413965114" LINK="https://www.youtube.com/watch?v=F-wbD9FSs2w&amp;list=PLoBnmHLDIN8-WO1VGhcuQqEjNssqn5trY&amp;index=2" MODIFIED="1648756232749" TEXT="Semin&#xe1;rio: diretividade e resposta ao impulso espacial"/>
+</node>
+<node CREATED="1648755917906" FOLDED="true" ID="ID_174762169" MODIFIED="1649074747814" TEXT="Delay and velocity measurements">
+<node CREATED="1648755947402" ID="ID_116389427" LINK="https://colab.research.google.com/drive/1b3fziel7O7XJ0S4p6uVQg0GNxLrP1X-a?usp=sharing" MODIFIED="1648755999246" TEXT="Script on Google Colab"/>
+</node>
+<node CREATED="1648756073144" FOLDED="true" ID="ID_150541094" MODIFIED="1649074756600" TEXT="Envelope detection (demodulation)">
+<node CREATED="1648756082051" ID="ID_1743627156" LINK="https://colab.research.google.com/drive/1ouewRk2yhCtD1wFCN4ZbFk74csJVzvfy?usp=sharing" MODIFIED="1648756090392" TEXT="Script on Google Colab"/>
+</node>
+<node CREATED="1651027784817" FOLDED="true" ID="ID_9875842" MODIFIED="1651028043303" TEXT="Effects of surface roughness">
+<node CREATED="1651027813353" FOLDED="true" ID="ID_1265363456" MODIFIED="1651028041123" TEXT="Nagy, Peter B., and James H. Rose. &quot;Surface roughness and the ultrasonic detection of subsurface scatterers.&quot; Journal of applied physics 73.2 (1993): 566-580.">
+<node CREATED="1651027882041" ID="ID_484389062" MODIFIED="1651027898951" TEXT="Especially Eqs. (9), (10) and (11)"/>
 </node>
 </node>
-<node CREATED="1606964165141" FOLDED="true" ID="ID_920771390" MODIFIED="1607520228741" POSITION="right" TEXT="Sinais e Sistemas">
+</node>
+<node CREATED="1606964165141" FOLDED="true" ID="ID_920771390" MODIFIED="1651027913471" POSITION="right" TEXT="Sinais e Sistemas">
 <node CREATED="1607517938795" HGAP="53" ID="ID_1585418671" LINK="https://www.youtube.com/watch?v=KJnAy6hzetw&amp;list=PL41692B571DD0AF9B&amp;ab_channel=MITOpenCourseWare" MODIFIED="1607517991581" VSHIFT="20">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -102,7 +124,7 @@
 <node CREATED="1606963560544" FOLDED="true" HGAP="22" ID="ID_81845382" MODIFIED="1617799218310" POSITION="right" TEXT="Identifica&#xe7;&#xe3;o de Sistemas" VSHIFT="-1">
 <node CREATED="1606963608382" ID="ID_1724472532" LINK="https://www.youtube.com/watch?v=TWdgSG0sMlQ&amp;list=PLALrL4i0Pz6DrrCkkJ-k-_S3qi1bFzUUu&amp;ab_channel=LuisAntonioAguirre" MODIFIED="1606964315043" TEXT="Modelagem e Identifica&#xe7;&#xe3;o&#xa;(Luis Fernando Aguirre - Youtube)"/>
 </node>
-<node CREATED="1606964273870" FOLDED="true" ID="ID_406252487" MODIFIED="1617799476401" POSITION="right" TEXT="Controladores L&#xf3;gicos Program&#xe1;veis">
+<node CREATED="1606964273870" FOLDED="true" ID="ID_406252487" MODIFIED="1649074780390" POSITION="right" TEXT="Controladores L&#xf3;gicos Program&#xe1;veis">
 <node CREATED="1606964284979" FOLDED="true" ID="ID_62288926" MODIFIED="1617799475596" TEXT="CodeSys">
 <node CREATED="1607520173718" ID="ID_1524829885" LINK="https://www.youtube.com/watch?v=78W8Kf8GLAk&amp;ab_channel=GVensino" MODIFIED="1607520256763">
 <richcontent TYPE="NODE"><html>
@@ -132,12 +154,28 @@
       (Tohid Alizadeh - YouTube)
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
-<node CREATED="1606963851473" FOLDED="true" ID="ID_226930671" MODIFIED="1617799226181" POSITION="right" TEXT="C&#xe1;lculo">
+<node CREATED="1648756268545" FOLDED="true" ID="ID_769954926" MODIFIED="1649074720105" POSITION="right" TEXT="Image reconstruction">
+<node CREATED="1648756284824" FOLDED="true" ID="ID_635881276" MODIFIED="1648756825556" TEXT="Regularization">
+<node CREATED="1648756542594" ID="ID_155755289" LINK="http://repositorio.utfpr.edu.br/jspui/bitstream/1/851/1/CT_CPGEI_M_Passarin%2c%20Thiago%20Alberto%20Rigo_2013.pdf" MODIFIED="1648756597401" TEXT="Disserta&#xe7;&#xe3;o de mestrado">
+<node CREATED="1648756632121" ID="ID_1314349294" MODIFIED="1648756669797" TEXT="3.2 Regulariza&#xe7;&#xe3;o em problemas inversos"/>
+</node>
+<node CREATED="1648756710910" ID="ID_1434154825" MODIFIED="1648756712733" TEXT="Karl, W. Clem. &quot;Regularization in image restoration and reconstruction.&quot; Handbook of Image and Video Processing. Academic Press, 2005. 183-V."/>
+</node>
+<node CREATED="1648756496714" FOLDED="true" ID="ID_290064650" MODIFIED="1648756827105" TEXT="Linear model-based reconstruction in ultrasound">
+<node CREATED="1648756542594" ID="ID_280590209" LINK="http://repositorio.utfpr.edu.br/jspui/bitstream/1/851/1/CT_CPGEI_M_Passarin%2c%20Thiago%20Alberto%20Rigo_2013.pdf" MODIFIED="1648756656010" TEXT="Disserta&#xe7;&#xe3;o de mestrado">
+<node CREATED="1648756570986" ID="ID_1406024620" MODIFIED="1648756571997" TEXT="2.6 Reconstru&#xe7;&#xe3;o por problemas inversos em ultrassom"/>
+<node CREATED="1648756577120" ID="ID_1133387909" MODIFIED="1648756578173" TEXT="3 Reconstru&#xe7;&#xe3;o de imagens por problemas inversos"/>
+</node>
+<node CREATED="1648756801177" ID="ID_930724742" MODIFIED="1648756802549" TEXT="Viola, Francesco, Michael A. Ellis, and William F. Walker. &quot;Time-domain optimized near-field estimator for ultrasound imaging: Initial development and results.&quot; IEEE Transactions on Medical Imaging 27.1 (2007): 99-110.">
+<node CREATED="1648756807138" ID="ID_405391722" MODIFIED="1648756811917" TEXT="See Fig. 2"/>
+</node>
+</node>
+</node>
+<node CREATED="1606963851473" FOLDED="true" ID="ID_226930671" MODIFIED="1648755908041" POSITION="right" TEXT="C&#xe1;lculo">
 <node CREATED="1606963900774" FOLDED="true" ID="ID_1325040626" MODIFIED="1617799222237" TEXT="C&#xe1;lculo multivari&#xe1;vel">
 <node CREATED="1606963857208" ID="ID_25741876" LINK="https://www.khanacademy.org/math/multivariable-calculus/applications-of-multivariable-derivatives#quadratic-approximations" MODIFIED="1606963965720">
 <richcontent TYPE="NODE"><html>
