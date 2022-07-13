7c57f9f3c1ab4e927bbe94a8529ef820609b4827
Thiago A. R. Passarin <38135428+thiagopas@users.noreply.github.com>
2022-07-12 23:08:30 -0400

    Add files via upload

--- topicos.mm
+++ topicos.mm
@@ -1,31 +1,7 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1606963364896" ID="ID_1941080162" MODIFIED="1606963543321" STYLE="bubble" TEXT="T&#xf3;picos">
-<node CREATED="1606963590938" FOLDED="true" ID="ID_1728845974" MODIFIED="1649074738301" POSITION="right" TEXT="Otimiza&#xe7;&#xe3;o">
-<node CREATED="1648756912593" ID="ID_1761385891" MODIFIED="1648756917717" TEXT="Adjoint state methods">
-<node CREATED="1648756919745" ID="ID_1813753556" MODIFIED="1648756920526" TEXT="Granzow, Glen D. &quot;A tutorial on adjoint methods and their use for data assimilation in glaciology.&quot; Journal of Glaciology 60.221 (2014): 440-446."/>
-</node>
-<node CREATED="1648756926121" ID="ID_609667712" MODIFIED="1648756931502" TEXT="Convex optimization">
-<node CREATED="1606964041319" ID="ID_1026839471" LINK="https://web.stanford.edu/~boyd/cvxbook/bv_cvxbook.pdf" MODIFIED="1606964230909">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      Convex Optimization (Boyd &amp; Vandenberghe)<br />(Livro em PDF)<br />
-    </p>
-  </body>
-</html></richcontent>
-</node>
-</node>
-<node CREATED="1648756974041" ID="ID_95634564" MODIFIED="1648756982469" TEXT="Greedy algorithms">
-<node CREATED="1648757114124" ID="ID_289126857" MODIFIED="1648757136676" TEXT="Orthogonal Matching Pursuit (OMP)">
-<node CREATED="1648757127666" ID="ID_1099533201" MODIFIED="1648757128445" TEXT="Tropp, Joel A., and Anna C. Gilbert. &quot;Signal recovery from random measurements via orthogonal matching pursuit.&quot; IEEE Transactions on information theory 53.12 (2007): 4655-4666."/>
-</node>
-</node>
-</node>
-<node CREATED="1607518523431" FOLDED="true" ID="ID_932147248" MODIFIED="1651028044624" POSITION="right" TEXT="Ac&#xfa;stica e Ultrassom">
+<node CREATED="1606963364896" ID="ID_1941080162" MODIFIED="1657681177916" STYLE="bubble" TEXT="T&#xf3;picos">
+<node CREATED="1607518523431" FOLDED="true" ID="ID_932147248" MODIFIED="1657681358016" POSITION="right" TEXT="Acoustics and Ultrasonics">
 <node CREATED="1607518803639" FOLDED="true" ID="ID_1410858501" MODIFIED="1649074764586" TEXT="Spatial impulse response">
 <node CREATED="1607518856783" ID="ID_799823470" LINK="http://bme.elektro.dtu.dk/31545/notes/ref_jaj_2015.pdf" MODIFIED="1607518939691">
 <richcontent TYPE="NODE"><html>
@@ -53,7 +29,130 @@
 </node>
 </node>
 </node>
-<node CREATED="1606964165141" FOLDED="true" ID="ID_920771390" MODIFIED="1651027913471" POSITION="right" TEXT="Sinais e Sistemas">
+<node CREATED="1606963851473" FOLDED="true" ID="ID_226930671" MODIFIED="1657681365315" POSITION="right" TEXT="Calculus">
+<node CREATED="1606963900774" FOLDED="true" ID="ID_1325040626" MODIFIED="1657681359455" TEXT="Multivariate calculus">
+<node CREATED="1606963857208" ID="ID_25741876" LINK="https://www.khanacademy.org/math/multivariable-calculus/applications-of-multivariable-derivatives#quadratic-approximations" MODIFIED="1606963965720">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Quadratic Representations<br />(Khan Academy - Youtube)<br />
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1617798977470" ID="ID_1406229378" LINK="https://www.youtube.com/watch?v=wCZ1VEmVjVo" MODIFIED="1617799139164">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      What is Jacobian? | The right way of
+    </p>
+    <p>
+      thinking derivatives and integrals
+    </p>
+    <p>
+      (Mathemaniac - YouTube)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1606963917192" FOLDED="true" ID="ID_1274649913" MODIFIED="1657681363537" TEXT="Calculus of variations">
+<node CREATED="1606963926639" ID="ID_1391634991" LINK="https://www.youtube.com/watch?v=6HeQc7CSkZs&amp;ab_channel=FacultyofKhan" MODIFIED="1606963990961">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Calculus of Variations
+    </p>
+    <p>
+      (Faculty of Khan - Youtube)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1606964514778" ID="ID_588094371" LINK="https://drive.google.com/file/d/1jg3eNaQx_T9ZsljRUR4bFFWpQZe6o0mu/view" MODIFIED="1606964556289">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      A practical introduction to Calculus of Variations
+    </p>
+    <p>
+      (Thiago Passarin - v&#237;deo)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1657680897102" FOLDED="true" ID="ID_69320932" MODIFIED="1657681371357" POSITION="right" TEXT="Control Theory">
+<node CREATED="1607518407584" FOLDED="true" ID="ID_410213140" MODIFIED="1657681369363" STYLE="bubble" TEXT="Laplace Transform">
+<node CREATED="1606964189651" FOLDED="true" ID="ID_85164201" MODIFIED="1657681367514">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Partial Fractions Expansion
+    </p>
+    <p>
+      for Inverse Laplace Transform
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1607518426514" ID="ID_207241993" LINK="https://lpsa.swarthmore.edu/BackGround/PartialFraction/PartialFraction.html" MODIFIED="1657681282398">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Partial Fraction Expansion
+    </p>
+    <p>
+      (Swarthmore)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1657681023138" ID="ID_259020213" MODIFIED="1657681028230" TEXT="Modern Control">
+<node CREATED="1657681037290" ID="ID_773485093" LINK="https://www.youtube.com/watch?v=Pi7l8mMjYVE&amp;list=PLMrJAkhIeNNR20Mz-VpzgfQs5zrYi085m" MODIFIED="1657681119069">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Control Bootcamp</b>
+    </p>
+    <p>
+      by Steve Brunton (Washington University)
+    </p>
+    <p>
+      (Playlist on Youtube)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1606964165141" FOLDED="true" ID="ID_920771390" MODIFIED="1657681375003" POSITION="right" TEXT="Fourier Analysis">
 <node CREATED="1607517938795" HGAP="53" ID="ID_1585418671" LINK="https://www.youtube.com/watch?v=KJnAy6hzetw&amp;list=PL41692B571DD0AF9B&amp;ab_channel=MITOpenCourseWare" MODIFIED="1607517991581" VSHIFT="20">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -69,7 +168,7 @@
   </body>
 </html></richcontent>
 </node>
-<node CREATED="1607518212953" FOLDED="true" HGAP="56" ID="ID_1365932305" MODIFIED="1607518483983" TEXT="S&#xe9;ries de Fourier" VSHIFT="16">
+<node CREATED="1607518212953" FOLDED="true" HGAP="56" ID="ID_1365932305" MODIFIED="1657680700081" TEXT="Fourier Series" VSHIFT="16">
 <node CREATED="1607518223795" ID="ID_658626383" LINK="https://lpsa.swarthmore.edu/Fourier/Series/ExFS.html" MODIFIED="1607518246704">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -86,7 +185,7 @@
 </html></richcontent>
 </node>
 </node>
-<node CREATED="1606964173899" FOLDED="true" HGAP="49" ID="ID_648621551" MODIFIED="1607518477263" TEXT="Diagramas de Bode" VSHIFT="21">
+<node CREATED="1606964173899" FOLDED="true" HGAP="49" ID="ID_648621551" MODIFIED="1657681373430" TEXT="Bode Diagrams" VSHIFT="21">
 <node CREATED="1607518286946" ID="ID_765408096" LINK="https://lpsa.swarthmore.edu/Bode/BodeHow.html" MODIFIED="1607518337919">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -115,16 +214,87 @@
 </html></richcontent>
 </node>
 </node>
-<node CREATED="1607518407584" FOLDED="true" ID="ID_410213140" MODIFIED="1607518481870" TEXT="Transformada de Laplace">
-<node CREATED="1606964189651" FOLDED="true" ID="ID_85164201" MODIFIED="1607518478491" TEXT="Fra&#xe7;&#xf5;es parciais para Transf. de Laplace">
-<node CREATED="1607518426514" ID="ID_207241993" LINK="https://lpsa.swarthmore.edu/BackGround/PartialFraction/PartialFraction.html" MODIFIED="1607518447050" TEXT="Partial Fraction Expansion"/>
+<node CREATED="1657680797154" ID="ID_831756448" LINK="https://www.youtube.com/watch?v=jNC0jxb0OxE&amp;list=PLMrJAkhIeNNT_Xh3Oy0Y4LTj0Oxo8GqsC" MODIFIED="1657681299150">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Fourier Analysis</b>
+    </p>
+    <p>
+      by Steve Brunton&#160;&#160;(Washington University)
+    </p>
+    <p>
+      (Playlist on Youtube)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1648756268545" FOLDED="true" ID="ID_769954926" MODIFIED="1657681229311" POSITION="right" TEXT="Image reconstruction">
+<node CREATED="1648756284824" FOLDED="true" ID="ID_635881276" MODIFIED="1648756825556" TEXT="Regularization">
+<node CREATED="1648756542594" ID="ID_155755289" LINK="http://repositorio.utfpr.edu.br/jspui/bitstream/1/851/1/CT_CPGEI_M_Passarin%2c%20Thiago%20Alberto%20Rigo_2013.pdf" MODIFIED="1648756597401" TEXT="Disserta&#xe7;&#xe3;o de mestrado">
+<node CREATED="1648756632121" ID="ID_1314349294" MODIFIED="1648756669797" TEXT="3.2 Regulariza&#xe7;&#xe3;o em problemas inversos"/>
+</node>
+<node CREATED="1648756710910" ID="ID_1434154825" MODIFIED="1648756712733" TEXT="Karl, W. Clem. &quot;Regularization in image restoration and reconstruction.&quot; Handbook of Image and Video Processing. Academic Press, 2005. 183-V."/>
+</node>
+<node CREATED="1648756496714" FOLDED="true" ID="ID_290064650" MODIFIED="1648756827105" TEXT="Linear model-based reconstruction in ultrasound">
+<node CREATED="1648756542594" ID="ID_280590209" LINK="http://repositorio.utfpr.edu.br/jspui/bitstream/1/851/1/CT_CPGEI_M_Passarin%2c%20Thiago%20Alberto%20Rigo_2013.pdf" MODIFIED="1648756656010" TEXT="Disserta&#xe7;&#xe3;o de mestrado">
+<node CREATED="1648756570986" ID="ID_1406024620" MODIFIED="1648756571997" TEXT="2.6 Reconstru&#xe7;&#xe3;o por problemas inversos em ultrassom"/>
+<node CREATED="1648756577120" ID="ID_1133387909" MODIFIED="1648756578173" TEXT="3 Reconstru&#xe7;&#xe3;o de imagens por problemas inversos"/>
+</node>
+<node CREATED="1648756801177" ID="ID_930724742" MODIFIED="1648756802549" TEXT="Viola, Francesco, Michael A. Ellis, and William F. Walker. &quot;Time-domain optimized near-field estimator for ultrasound imaging: Initial development and results.&quot; IEEE Transactions on Medical Imaging 27.1 (2007): 99-110.">
+<node CREATED="1648756807138" ID="ID_405391722" MODIFIED="1648756811917" TEXT="See Fig. 2"/>
 </node>
 </node>
 </node>
-<node CREATED="1606963560544" FOLDED="true" HGAP="22" ID="ID_81845382" MODIFIED="1617799218310" POSITION="right" TEXT="Identifica&#xe7;&#xe3;o de Sistemas" VSHIFT="-1">
-<node CREATED="1606963608382" ID="ID_1724472532" LINK="https://www.youtube.com/watch?v=TWdgSG0sMlQ&amp;list=PLALrL4i0Pz6DrrCkkJ-k-_S3qi1bFzUUu&amp;ab_channel=LuisAntonioAguirre" MODIFIED="1606964315043" TEXT="Modelagem e Identifica&#xe7;&#xe3;o&#xa;(Luis Fernando Aguirre - Youtube)"/>
+<node CREATED="1613997275996" FOLDED="true" ID="ID_1928652108" MODIFIED="1657681246337" POSITION="right" TEXT="Matlab">
+<node CREATED="1613997287096" ID="ID_1463047083" MODIFIED="1613997442687">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      GNU Octave
+    </p>
+    <p>
+      (Sistema em c&#243;digo aberto compat&#237;vel com Matlab)
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1613997296974" ID="ID_1711921887" LINK="https://octave-online.net/" MODIFIED="1613997330499" TEXT="Octave online"/>
+<node CREATED="1613997311414" ID="ID_239783632" LINK="https://en.wikipedia.org/wiki/GNU_Octave" MODIFIED="1613997375187" TEXT="GNU Octave (Wikipedia)"/>
 </node>
-<node CREATED="1606964273870" FOLDED="true" ID="ID_406252487" MODIFIED="1649074780390" POSITION="right" TEXT="Controladores L&#xf3;gicos Program&#xe1;veis">
+</node>
+<node CREATED="1606963590938" FOLDED="true" ID="ID_1728845974" MODIFIED="1657681387331" POSITION="right" TEXT="Optimization">
+<node CREATED="1648756912593" FOLDED="true" ID="ID_1761385891" MODIFIED="1657681377381" TEXT="Adjoint state methods">
+<node CREATED="1648756919745" ID="ID_1813753556" MODIFIED="1648756920526" TEXT="Granzow, Glen D. &quot;A tutorial on adjoint methods and their use for data assimilation in glaciology.&quot; Journal of Glaciology 60.221 (2014): 440-446."/>
+</node>
+<node CREATED="1648756926121" FOLDED="true" ID="ID_609667712" MODIFIED="1657681378531" TEXT="Convex optimization">
+<node CREATED="1606964041319" ID="ID_1026839471" LINK="https://web.stanford.edu/~boyd/cvxbook/bv_cvxbook.pdf" MODIFIED="1606964230909">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Convex Optimization (Boyd &amp; Vandenberghe)<br />(Livro em PDF)<br />
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1648756974041" FOLDED="true" ID="ID_95634564" MODIFIED="1657681385652" TEXT="Greedy algorithms">
+<node CREATED="1648757114124" FOLDED="true" ID="ID_289126857" MODIFIED="1657681384094" TEXT="Orthogonal Matching Pursuit (OMP)">
+<node CREATED="1648757127666" ID="ID_1099533201" MODIFIED="1648757128445" TEXT="Tropp, Joel A., and Anna C. Gilbert. &quot;Signal recovery from random measurements via orthogonal matching pursuit.&quot; IEEE Transactions on information theory 53.12 (2007): 4655-4666."/>
+</node>
+</node>
+</node>
+<node CREATED="1606964273870" FOLDED="true" ID="ID_406252487" MODIFIED="1657681220430" POSITION="right" TEXT="Programmable Logic Controllers">
 <node CREATED="1606964284979" FOLDED="true" ID="ID_62288926" MODIFIED="1617799475596" TEXT="CodeSys">
 <node CREATED="1607520173718" ID="ID_1524829885" LINK="https://www.youtube.com/watch?v=78W8Kf8GLAk&amp;ab_channel=GVensino" MODIFIED="1607520256763">
 <richcontent TYPE="NODE"><html>
@@ -158,107 +328,33 @@
 </node>
 </node>
 </node>
-<node CREATED="1648756268545" FOLDED="true" ID="ID_769954926" MODIFIED="1649074720105" POSITION="right" TEXT="Image reconstruction">
-<node CREATED="1648756284824" FOLDED="true" ID="ID_635881276" MODIFIED="1648756825556" TEXT="Regularization">
-<node CREATED="1648756542594" ID="ID_155755289" LINK="http://repositorio.utfpr.edu.br/jspui/bitstream/1/851/1/CT_CPGEI_M_Passarin%2c%20Thiago%20Alberto%20Rigo_2013.pdf" MODIFIED="1648756597401" TEXT="Disserta&#xe7;&#xe3;o de mestrado">
-<node CREATED="1648756632121" ID="ID_1314349294" MODIFIED="1648756669797" TEXT="3.2 Regulariza&#xe7;&#xe3;o em problemas inversos"/>
-</node>
-<node CREATED="1648756710910" ID="ID_1434154825" MODIFIED="1648756712733" TEXT="Karl, W. Clem. &quot;Regularization in image restoration and reconstruction.&quot; Handbook of Image and Video Processing. Academic Press, 2005. 183-V."/>
-</node>
-<node CREATED="1648756496714" FOLDED="true" ID="ID_290064650" MODIFIED="1648756827105" TEXT="Linear model-based reconstruction in ultrasound">
-<node CREATED="1648756542594" ID="ID_280590209" LINK="http://repositorio.utfpr.edu.br/jspui/bitstream/1/851/1/CT_CPGEI_M_Passarin%2c%20Thiago%20Alberto%20Rigo_2013.pdf" MODIFIED="1648756656010" TEXT="Disserta&#xe7;&#xe3;o de mestrado">
-<node CREATED="1648756570986" ID="ID_1406024620" MODIFIED="1648756571997" TEXT="2.6 Reconstru&#xe7;&#xe3;o por problemas inversos em ultrassom"/>
-<node CREATED="1648756577120" ID="ID_1133387909" MODIFIED="1648756578173" TEXT="3 Reconstru&#xe7;&#xe3;o de imagens por problemas inversos"/>
-</node>
-<node CREATED="1648756801177" ID="ID_930724742" MODIFIED="1648756802549" TEXT="Viola, Francesco, Michael A. Ellis, and William F. Walker. &quot;Time-domain optimized near-field estimator for ultrasound imaging: Initial development and results.&quot; IEEE Transactions on Medical Imaging 27.1 (2007): 99-110.">
-<node CREATED="1648756807138" ID="ID_405391722" MODIFIED="1648756811917" TEXT="See Fig. 2"/>
-</node>
-</node>
-</node>
-<node CREATED="1606963851473" FOLDED="true" ID="ID_226930671" MODIFIED="1648755908041" POSITION="right" TEXT="C&#xe1;lculo">
-<node CREATED="1606963900774" FOLDED="true" ID="ID_1325040626" MODIFIED="1617799222237" TEXT="C&#xe1;lculo multivari&#xe1;vel">
-<node CREATED="1606963857208" ID="ID_25741876" LINK="https://www.khanacademy.org/math/multivariable-calculus/applications-of-multivariable-derivatives#quadratic-approximations" MODIFIED="1606963965720">
+<node CREATED="1657680000814" FOLDED="true" ID="ID_1759381397" MODIFIED="1657681396580" POSITION="right" TEXT="Python">
+<node CREATED="1657680075759" FOLDED="true" ID="ID_1329184105" MODIFIED="1657681391691" TEXT="Multithreading">
+<node CREATED="1657680092027" ID="ID_1659775600" LINK="https://realpython.com/intro-to-python-threading/" MODIFIED="1657680138359">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      Quadratic Representations<br />(Khan Academy - Youtube)<br />
-    </p>
-  </body>
-</html></richcontent>
-</node>
-<node CREATED="1617798977470" ID="ID_1406229378" LINK="https://www.youtube.com/watch?v=wCZ1VEmVjVo" MODIFIED="1617799139164">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      What is Jacobian? | The right way of
+      An Intro to Threading in Python
     </p>
     <p>
-      thinking derivatives and integrals
+      by Jim Anderson
     </p>
     <p>
-      (Mathemaniac - YouTube)
+      realpython.com
     </p>
   </body>
 </html></richcontent>
 </node>
 </node>
-<node CREATED="1606963917192" FOLDED="true" ID="ID_1274649913" MODIFIED="1617799185656" TEXT="C&#xe1;lculo Variacional">
-<node CREATED="1606963926639" ID="ID_1391634991" LINK="https://www.youtube.com/watch?v=6HeQc7CSkZs&amp;ab_channel=FacultyofKhan" MODIFIED="1606963990961">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      Calculus of Variations
-    </p>
-    <p>
-      (Faculty of Khan - Youtube)
-    </p>
-  </body>
-</html></richcontent>
-</node>
-<node CREATED="1606964514778" ID="ID_588094371" LINK="https://drive.google.com/file/d/1jg3eNaQx_T9ZsljRUR4bFFWpQZe6o0mu/view" MODIFIED="1606964556289">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      A practical introduction to Calculus of Variations
-    </p>
-    <p>
-      (Thiago Passarin - v&#237;deo)
-    </p>
-  </body>
-</html></richcontent>
+<node CREATED="1657680174203" FOLDED="true" ID="ID_1519542786" MODIFIED="1657681393217" TEXT="Python on Android">
+<node CREATED="1657680292422" ID="ID_1518886704" LINK="https://docs.google.com/document/d/e/2PACX-1vQuzy59eA4TGLQakzg99uQ6EO9Rn-JpQ8bNW4zaQLbVz5j91dukHpkKfNrv8Sg2s3XCXOvZk0oJELAJ/pub" MODIFIED="1657680515164" TEXT="Tutorial: Termux+Python"/>
 </node>
 </node>
-</node>
-<node CREATED="1613997275996" FOLDED="true" ID="ID_1928652108" MODIFIED="1617799194602" POSITION="right" TEXT="Matlab">
-<node CREATED="1613997287096" ID="ID_1463047083" MODIFIED="1613997442687">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      GNU Octave
-    </p>
-    <p>
-      (Sistema em c&#243;digo aberto compat&#237;vel com Matlab)
-    </p>
-  </body>
-</html></richcontent>
-<node CREATED="1613997296974" ID="ID_1711921887" LINK="https://octave-online.net/" MODIFIED="1613997330499" TEXT="Octave online"/>
-<node CREATED="1613997311414" ID="ID_239783632" LINK="https://en.wikipedia.org/wiki/GNU_Octave" MODIFIED="1613997375187" TEXT="GNU Octave (Wikipedia)"/>
-</node>
+<node CREATED="1606963560544" FOLDED="true" HGAP="22" ID="ID_81845382" MODIFIED="1657680647227" POSITION="right" TEXT="System Identification" VSHIFT="-1">
+<node CREATED="1606963608382" ID="ID_1724472532" LINK="https://www.youtube.com/watch?v=TWdgSG0sMlQ&amp;list=PLALrL4i0Pz6DrrCkkJ-k-_S3qi1bFzUUu&amp;ab_channel=LuisAntonioAguirre" MODIFIED="1606964315043" TEXT="Modelagem e Identifica&#xe7;&#xe3;o&#xa;(Luis Fernando Aguirre - Youtube)"/>
 </node>
 </node>
 </map>
