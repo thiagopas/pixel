02070c58a5e43373cc4587be7a92d98c941f4b6a
Thiago A. R. Passarin <38135428+thiagopas@users.noreply.github.com>
2022-09-05 14:47:15 -0300

    Add files via upload

--- topicos.mm
+++ topicos.mm
@@ -1,7 +1,7 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1606963364896" ID="ID_1941080162" MODIFIED="1657683137181" STYLE="bubble" TEXT="Topics">
-<node CREATED="1607518523431" FOLDED="true" ID="ID_932147248" MODIFIED="1657681358016" POSITION="right" TEXT="Acoustics and Ultrasonics">
+<node CREATED="1607518523431" FOLDED="true" ID="ID_932147248" MODIFIED="1662399699935" POSITION="right" TEXT="Acoustics and Ultrasonics">
 <node CREATED="1607518803639" FOLDED="true" ID="ID_1410858501" MODIFIED="1649074764586" TEXT="Spatial impulse response">
 <node CREATED="1607518856783" ID="ID_799823470" LINK="http://bme.elektro.dtu.dk/31545/notes/ref_jaj_2015.pdf" MODIFIED="1607518939691">
 <richcontent TYPE="NODE"><html>
@@ -17,20 +17,23 @@
 </node>
 <node CREATED="1648756207394" ID="ID_1413965114" LINK="https://www.youtube.com/watch?v=F-wbD9FSs2w&amp;list=PLoBnmHLDIN8-WO1VGhcuQqEjNssqn5trY&amp;index=2" MODIFIED="1648756232749" TEXT="Semin&#xe1;rio: diretividade e resposta ao impulso espacial"/>
 </node>
-<node CREATED="1648755917906" FOLDED="true" ID="ID_174762169" MODIFIED="1649074747814" TEXT="Delay and velocity measurements">
+<node CREATED="1648755917906" FOLDED="true" ID="ID_174762169" MODIFIED="1662399692897" TEXT="Delay and velocity measurements">
 <node CREATED="1648755947402" ID="ID_116389427" LINK="https://colab.research.google.com/drive/1b3fziel7O7XJ0S4p6uVQg0GNxLrP1X-a?usp=sharing" MODIFIED="1648755999246" TEXT="Script on Google Colab"/>
 </node>
-<node CREATED="1648756073144" FOLDED="true" ID="ID_150541094" MODIFIED="1649074756600" TEXT="Envelope detection (demodulation)">
+<node CREATED="1648756073144" FOLDED="true" ID="ID_150541094" MODIFIED="1662399695455" TEXT="Envelope detection (demodulation)">
 <node CREATED="1648756082051" ID="ID_1743627156" LINK="https://colab.research.google.com/drive/1ouewRk2yhCtD1wFCN4ZbFk74csJVzvfy?usp=sharing" MODIFIED="1648756090392" TEXT="Script on Google Colab"/>
 </node>
-<node CREATED="1651027784817" FOLDED="true" ID="ID_9875842" MODIFIED="1651028043303" TEXT="Effects of surface roughness">
+<node CREATED="1651027784817" FOLDED="true" ID="ID_9875842" MODIFIED="1662399698498" TEXT="Effects of surface roughness">
 <node CREATED="1651027813353" FOLDED="true" ID="ID_1265363456" MODIFIED="1651028041123" TEXT="Nagy, Peter B., and James H. Rose. &quot;Surface roughness and the ultrasonic detection of subsurface scatterers.&quot; Journal of applied physics 73.2 (1993): 566-580.">
 <node CREATED="1651027882041" ID="ID_484389062" MODIFIED="1651027898951" TEXT="Especially Eqs. (9), (10) and (11)"/>
 </node>
 </node>
 </node>
-<node CREATED="1606963851473" FOLDED="true" ID="ID_226930671" MODIFIED="1657681365315" POSITION="right" TEXT="Calculus">
-<node CREATED="1606963900774" FOLDED="true" ID="ID_1325040626" MODIFIED="1657681359455" TEXT="Multivariate calculus">
+<node CREATED="1662399546984" FOLDED="true" ID="ID_281303066" MODIFIED="1662399680445" POSITION="right" TEXT="Artificial Neural Networks">
+<node CREATED="1662399668800" ID="ID_1533826964" LINK="https://spectrum.ieee.org/what-is-deep-learning" MODIFIED="1662399678748" TEXT="Moore, Samuel K., David Schneider, and Eliza Strickland. &quot;How Deep Learning Works: Inside the Neural Networks that Power Today&apos;s AI.&quot; IEEE Spectrum 58.10 (2021): 32-33."/>
+</node>
+<node CREATED="1606963851473" FOLDED="true" ID="ID_226930671" MODIFIED="1662399197892" POSITION="right" TEXT="Calculus">
+<node CREATED="1606963900774" FOLDED="true" ID="ID_1325040626" MODIFIED="1662399187033" TEXT="Multivariate calculus">
 <node CREATED="1606963857208" ID="ID_25741876" LINK="https://www.khanacademy.org/math/multivariable-calculus/applications-of-multivariable-derivatives#quadratic-approximations" MODIFIED="1606963965720">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -62,7 +65,7 @@
 </html></richcontent>
 </node>
 </node>
-<node CREATED="1606963917192" FOLDED="true" ID="ID_1274649913" MODIFIED="1657681363537" TEXT="Calculus of variations">
+<node CREATED="1606963917192" FOLDED="true" ID="ID_1274649913" MODIFIED="1662399194254" TEXT="Calculus of variations">
 <node CREATED="1606963926639" ID="ID_1391634991" LINK="https://www.youtube.com/watch?v=6HeQc7CSkZs&amp;ab_channel=FacultyofKhan" MODIFIED="1606963990961">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -95,9 +98,47 @@
 </node>
 </node>
 </node>
-<node CREATED="1657680897102" FOLDED="true" ID="ID_69320932" MODIFIED="1657681371357" POSITION="right" TEXT="Control Theory">
-<node CREATED="1607518407584" FOLDED="true" ID="ID_410213140" MODIFIED="1657681369363" STYLE="bubble" TEXT="Laplace Transform">
-<node CREATED="1606964189651" FOLDED="true" ID="ID_85164201" MODIFIED="1657681367514">
+<node CREATED="1662398023543" FOLDED="true" ID="ID_1916756950" MODIFIED="1662399920579" POSITION="right" TEXT="Computer vision">
+<node CREATED="1662398036913" FOLDED="true" ID="ID_1979618838" MODIFIED="1662399919061" TEXT="Rotation and translation estimation">
+<node CREATED="1662398482193" ID="ID_824737818" LINK="https://rotations.berkeley.edu/estimating-rotations-and-translations-from-optical-targets/" MODIFIED="1662399914058">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Estimating rotations and translations from optical targets
+    </p>
+    <p>
+      (University of Kaliforcia, Berkeley)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1662399772151" FOLDED="true" ID="ID_1286839168" MODIFIED="1662399812723" TEXT="Global displacement estimation">
+<node CREATED="1662399781888" ID="ID_1242391755" LINK="https://en.wikipedia.org/wiki/Phase_correlation" MODIFIED="1662399805307">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Phase Correlation Algorithm
+    </p>
+    <p>
+      (Wikipedia)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1657680897102" FOLDED="true" ID="ID_69320932" MODIFIED="1662399464362" POSITION="right" TEXT="Control Theory">
+<node CREATED="1607518407584" FOLDED="true" ID="ID_410213140" MODIFIED="1662399461548" STYLE="bubble" TEXT="Laplace Transform">
+<node CREATED="1606964189651" FOLDED="true" ID="ID_85164201" MODIFIED="1662399460563">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -110,8 +151,7 @@
       for Inverse Laplace Transform
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1607518426514" ID="ID_207241993" LINK="https://lpsa.swarthmore.edu/BackGround/PartialFraction/PartialFraction.html" MODIFIED="1657681282398">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -125,12 +165,11 @@
       (Swarthmore)
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
-<node CREATED="1657681023138" ID="ID_259020213" MODIFIED="1657681028230" TEXT="Modern Control">
+<node CREATED="1657681023138" FOLDED="true" ID="ID_259020213" MODIFIED="1662399454953" TEXT="Modern Control">
 <node CREATED="1657681037290" ID="ID_773485093" LINK="https://www.youtube.com/watch?v=Pi7l8mMjYVE&amp;list=PLMrJAkhIeNNR20Mz-VpzgfQs5zrYi085m" MODIFIED="1657681119069">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -147,12 +186,29 @@
       (Playlist on Youtube)
     </p>
   </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1606963560544" FOLDED="true" HGAP="22" ID="ID_81845382" MODIFIED="1662399449920" TEXT="System Identification" VSHIFT="-1">
+<node CREATED="1606963608382" ID="ID_1724472532" LINK="https://www.youtube.com/watch?v=TWdgSG0sMlQ&amp;list=PLALrL4i0Pz6DrrCkkJ-k-_S3qi1bFzUUu&amp;ab_channel=LuisAntonioAguirre" MODIFIED="1606964315043" TEXT="Modelagem e Identifica&#xe7;&#xe3;o&#xa;(Luis Fernando Aguirre - Youtube)"/>
+</node>
+<node CREATED="1662399351544" FOLDED="true" ID="ID_1836055849" MODIFIED="1662399440227" TEXT="Networked Control Systems">
+<node CREATED="1662399365176" ID="ID_523945111" LINK="https://doi.org/10.1109/JPROC.2006.887288" MODIFIED="1662399438280">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Hespanha, Joo P., Payam Naghshtabrizi, and Yonggang Xu. &quot;A survey of recent results in networked control systems.&quot; Proceedings of the IEEE 95.1 (2007): 138-162.
+    </p>
+  </body>
 </html>
 </richcontent>
 </node>
 </node>
 </node>
-<node CREATED="1606964165141" FOLDED="true" ID="ID_920771390" MODIFIED="1657681375003" POSITION="right" TEXT="Fourier Analysis">
+<node CREATED="1606964165141" FOLDED="true" ID="ID_920771390" MODIFIED="1662398399649" POSITION="right" TEXT="Fourier Analysis">
 <node CREATED="1607517938795" HGAP="53" ID="ID_1585418671" LINK="https://www.youtube.com/watch?v=KJnAy6hzetw&amp;list=PL41692B571DD0AF9B&amp;ab_channel=MITOpenCourseWare" MODIFIED="1607517991581" VSHIFT="20">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -230,11 +286,10 @@
       (Playlist on Youtube)
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
-<node CREATED="1648756268545" FOLDED="true" ID="ID_769954926" MODIFIED="1657681229311" POSITION="right" TEXT="Image reconstruction">
+<node CREATED="1648756268545" FOLDED="true" ID="ID_769954926" MODIFIED="1662399159760" POSITION="right" TEXT="Image reconstruction">
 <node CREATED="1648756284824" FOLDED="true" ID="ID_635881276" MODIFIED="1648756825556" TEXT="Regularization">
 <node CREATED="1648756542594" ID="ID_155755289" LINK="http://repositorio.utfpr.edu.br/jspui/bitstream/1/851/1/CT_CPGEI_M_Passarin%2c%20Thiago%20Alberto%20Rigo_2013.pdf" MODIFIED="1648756597401" TEXT="Disserta&#xe7;&#xe3;o de mestrado">
 <node CREATED="1648756632121" ID="ID_1314349294" MODIFIED="1648756669797" TEXT="3.2 Regulariza&#xe7;&#xe3;o em problemas inversos"/>
@@ -250,8 +305,17 @@
 <node CREATED="1648756807138" ID="ID_405391722" MODIFIED="1648756811917" TEXT="See Fig. 2"/>
 </node>
 </node>
+<node CREATED="1662398599521" ID="ID_456515333" MODIFIED="1662398655978" TEXT="Full Waveform Inversion">
+<node CREATED="1662398624153" ID="ID_1893546160" MODIFIED="1662398648287" TEXT="See Optimization &gt; Adjoint State Methods">
+<arrowlink DESTINATION="ID_1893546160" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_165812937" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1893546160" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_165812937" SOURCE="ID_1893546160" STARTARROW="None" STARTINCLINATION="0;0;"/>
 </node>
-<node CREATED="1613997275996" FOLDED="true" ID="ID_1928652108" MODIFIED="1657681246337" POSITION="right" TEXT="Matlab">
+<node CREATED="1662398920311" FOLDED="true" ID="ID_1295975730" LINK="https://doi.org/10.1190/1.1442188" MODIFIED="1662399149893" TEXT="Gauthier, Odile, Jean Virieux, and Albert Tarantola. &quot;Two-dimensional nonlinear inversion of seismic waveforms: Numerical results.&quot; Geophysics 51.7 (1986): 1387-1403.">
+<node CREATED="1662398963992" ID="ID_988280886" MODIFIED="1662399148022" STYLE="bubble" TEXT="This is a very didactic, tutorial-like paper that may serve to those giving a first look at FWI"/>
+</node>
+</node>
+</node>
+<node CREATED="1613997275996" FOLDED="true" ID="ID_1928652108" MODIFIED="1662399220896" POSITION="right" TEXT="Matlab">
 <node CREATED="1613997287096" ID="ID_1463047083" MODIFIED="1613997442687">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -270,11 +334,11 @@
 <node CREATED="1613997311414" ID="ID_239783632" LINK="https://en.wikipedia.org/wiki/GNU_Octave" MODIFIED="1613997375187" TEXT="GNU Octave (Wikipedia)"/>
 </node>
 </node>
-<node CREATED="1606963590938" FOLDED="true" ID="ID_1728845974" MODIFIED="1657681387331" POSITION="right" TEXT="Optimization">
-<node CREATED="1648756912593" FOLDED="true" ID="ID_1761385891" MODIFIED="1657681377381" TEXT="Adjoint state methods">
-<node CREATED="1648756919745" ID="ID_1813753556" MODIFIED="1648756920526" TEXT="Granzow, Glen D. &quot;A tutorial on adjoint methods and their use for data assimilation in glaciology.&quot; Journal of Glaciology 60.221 (2014): 440-446."/>
+<node CREATED="1606963590938" FOLDED="true" ID="ID_1728845974" MODIFIED="1662399292624" POSITION="right" TEXT="Optimization">
+<node CREATED="1648756912593" FOLDED="true" ID="ID_1761385891" MODIFIED="1662399236347" TEXT="Adjoint state methods">
+<node CREATED="1648756919745" ID="ID_1813753556" LINK="https://www.cambridge.org/core/services/aop-cambridge-core/content/view/926428B3ACFAEA277DED39A1896A8109/S0022143000205911a.pdf/a-tutorial-on-adjoint-methods-and-their-use-for-data-assimilation-in-glaciology.pdf" MODIFIED="1662399131752" TEXT="Granzow, Glen D. &quot;A tutorial on adjoint methods and their use for data assimilation in glaciology.&quot; Journal of Glaciology 60.221 (2014): 440-446."/>
 </node>
-<node CREATED="1648756926121" FOLDED="true" ID="ID_609667712" MODIFIED="1657681378531" TEXT="Convex optimization">
+<node CREATED="1648756926121" FOLDED="true" ID="ID_609667712" MODIFIED="1662399238821" TEXT="Convex optimization">
 <node CREATED="1606964041319" ID="ID_1026839471" LINK="https://web.stanford.edu/~boyd/cvxbook/bv_cvxbook.pdf" MODIFIED="1606964230909">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -288,14 +352,14 @@
 </html></richcontent>
 </node>
 </node>
-<node CREATED="1648756974041" FOLDED="true" ID="ID_95634564" MODIFIED="1657681385652" TEXT="Greedy algorithms">
-<node CREATED="1648757114124" FOLDED="true" ID="ID_289126857" MODIFIED="1657681384094" TEXT="Orthogonal Matching Pursuit (OMP)">
-<node CREATED="1648757127666" ID="ID_1099533201" MODIFIED="1648757128445" TEXT="Tropp, Joel A., and Anna C. Gilbert. &quot;Signal recovery from random measurements via orthogonal matching pursuit.&quot; IEEE Transactions on information theory 53.12 (2007): 4655-4666."/>
+<node CREATED="1648756974041" FOLDED="true" ID="ID_95634564" MODIFIED="1662399291437" TEXT="Greedy algorithms">
+<node CREATED="1648757114124" FOLDED="true" ID="ID_289126857" MODIFIED="1662399290391" TEXT="Orthogonal Matching Pursuit (OMP)">
+<node CREATED="1648757127666" ID="ID_1099533201" LINK="https://doi.org/10.1109/TIT.2007.909108" MODIFIED="1662399281116" TEXT="Tropp, Joel A., and Anna C. Gilbert. &quot;Signal recovery from random measurements via orthogonal matching pursuit.&quot; IEEE Transactions on information theory 53.12 (2007): 4655-4666."/>
 </node>
 </node>
 </node>
-<node CREATED="1606964273870" FOLDED="true" ID="ID_406252487" MODIFIED="1657681220430" POSITION="right" TEXT="Programmable Logic Controllers">
-<node CREATED="1606964284979" FOLDED="true" ID="ID_62288926" MODIFIED="1617799475596" TEXT="CodeSys">
+<node CREATED="1606964273870" FOLDED="true" ID="ID_406252487" MODIFIED="1662399228159" POSITION="right" TEXT="Programmable Logic Controllers">
+<node CREATED="1606964284979" FOLDED="true" ID="ID_62288926" MODIFIED="1662399166743" TEXT="CodeSys">
 <node CREATED="1607520173718" ID="ID_1524829885" LINK="https://www.youtube.com/watch?v=78W8Kf8GLAk&amp;ab_channel=GVensino" MODIFIED="1607520256763">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -328,8 +392,8 @@
 </node>
 </node>
 </node>
-<node CREATED="1657680000814" FOLDED="true" ID="ID_1759381397" MODIFIED="1657681396580" POSITION="right" TEXT="Python">
-<node CREATED="1657680075759" FOLDED="true" ID="ID_1329184105" MODIFIED="1657681391691" TEXT="Multithreading">
+<node CREATED="1657680000814" FOLDED="true" ID="ID_1759381397" MODIFIED="1662399230990" POSITION="right" TEXT="Python">
+<node CREATED="1657680075759" FOLDED="true" ID="ID_1329184105" MODIFIED="1662399175189" TEXT="Multithreading">
 <node CREATED="1657680092027" ID="ID_1659775600" LINK="https://realpython.com/intro-to-python-threading/" MODIFIED="1657680138359">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -349,12 +413,9 @@
 </html></richcontent>
 </node>
 </node>
-<node CREATED="1657680174203" FOLDED="true" ID="ID_1519542786" MODIFIED="1657681393217" TEXT="Python on Android">
+<node CREATED="1657680174203" FOLDED="true" ID="ID_1519542786" MODIFIED="1662399173469" TEXT="Python on Android">
 <node CREATED="1657680292422" ID="ID_1518886704" LINK="https://docs.google.com/document/d/e/2PACX-1vQuzy59eA4TGLQakzg99uQ6EO9Rn-JpQ8bNW4zaQLbVz5j91dukHpkKfNrv8Sg2s3XCXOvZk0oJELAJ/pub" MODIFIED="1657680515164" TEXT="Tutorial: Termux+Python"/>
 </node>
-</node>
-<node CREATED="1606963560544" FOLDED="true" HGAP="22" ID="ID_81845382" MODIFIED="1657680647227" POSITION="right" TEXT="System Identification" VSHIFT="-1">
-<node CREATED="1606963608382" ID="ID_1724472532" LINK="https://www.youtube.com/watch?v=TWdgSG0sMlQ&amp;list=PLALrL4i0Pz6DrrCkkJ-k-_S3qi1bFzUUu&amp;ab_channel=LuisAntonioAguirre" MODIFIED="1606964315043" TEXT="Modelagem e Identifica&#xe7;&#xe3;o&#xa;(Luis Fernando Aguirre - Youtube)"/>
 </node>
 </node>
 </map>
